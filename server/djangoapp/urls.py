dded52413f22779bde68e6c3b62fd078c9f0ffdd
izelbia1-lgtm <izelbia1@gmail.com>
2026-04-20 12:23:15 +0200

    Complete Lab 2 user management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_request, name='logout'),
+    path('register', views.registration, name='register'),
     path('get_cars', views.get_cars, name='getcars'),
 ]
