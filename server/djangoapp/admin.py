3d3ec0224e0bf31d066724cc8f1294e95eaafc44
izelbia1-lgtm <izelbia1@gmail.com>
2026-04-20 13:59:51 +0200

    Complete Lab 4 car makes and models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,24 +1,5 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
-# Inline model for CarModel inside CarMake
-class CarModelInline(admin.TabularInline):
-    model = CarModel
-    extra = 1
-
-
-# Admin for CarModel
-class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'year', 'dealer_id')
-
-
-# Admin for CarMake with inline CarModels
-class CarMakeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    inlines = [CarModelInline]
-
-
-# Register models
-admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake)
+admin.site.register(CarModel)
