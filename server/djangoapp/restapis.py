77d4c5ae852c8f34d4ad420fabaac1afa297a66c
izelbia1-lgtm <izelbia1@gmail.com>
2026-05-04 11:01:03 +0200

    Complete capstone dealer reviews app

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://127.0.0.1:5050")
 
 
 def get_request(endpoint, **kwargs):
